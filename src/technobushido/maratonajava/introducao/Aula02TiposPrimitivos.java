65af47e46d1b9fd0611be112a7ec162f38356159
José Lourenco <josern1771@gmail.com>
2026-07-27 19:00:10 -0300

    Tipos primitivos

---
+++ src/technobushido/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package technobushido.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long e boolean
+        int idade = 10;
+        double salarioDouble = 100.0;
+        float salarioFloat = 2500.00F;
+        char caractere = 'a';
+        byte idadebyte = 10;
+        short idadeShort = 25;
+        long numeroGrande = 100000000000000L;
+        boolean verdadeiroOuFalso = true;
+
+        System.out.println("idade " + idade );
+    }
+
+}
