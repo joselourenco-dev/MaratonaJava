720be06e9f43437172d26eece38a455bfb023786
José Lourenco <josern1771@gmail.com>
2026-08-27 15:10:07 -0300

    Operaor ternario

--- src/technobushido/maratonajava/introducao/OperadorTernario.java
+++ src/technobushido/maratonajava/introducao/OperadorTernario.java
@@ -1,4 +1,12 @@
 package technobushido.maratonajava.introducao;
 
 public class OperadorTernario {
+    static void main() {
+        double salario = 400.0;
+        String doar = "Irei doar";
+        String naoDoar = "Não irei doar";
+        String resultado = salario >= 500 ? doar : naoDoar;
+
+        System.out.println(resultado);
+    }
 }
