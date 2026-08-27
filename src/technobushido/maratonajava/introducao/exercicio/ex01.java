8313a4ea2ae4080a5507b346a42a569cac82d474
José Lourenco <josern1771@gmail.com>
2026-08-27 14:05:04 -0300

    Exercicio tipos primitivos

---
+++ src/technobushido/maratonajava/introducao/exercicio/ex01.java
@@ -0,0 +1,16 @@
+package technobushido.maratonajava.introducao.exercicio;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class ex01 {
+    public static void main(String[] args){
+        String nome = "Samurai";
+        String endereço = "Rua 01, centro - SP";
+        double salario = 1786.76;
+        String dataRecebimentoSalario = "20/09/2022";
+
+        System.out.println("Eu " + nome + " morando no endereço " + endereço + " confirmo que recebi o sálario no vanor de " + salario +" na dataRecebimentoSalario " + dataRecebimentoSalario );
+
+    }
+}
