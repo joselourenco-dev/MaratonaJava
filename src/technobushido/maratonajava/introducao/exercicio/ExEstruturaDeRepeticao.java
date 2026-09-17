aa068caaca39611ab186caa20a22c584f16e389d
José Lourenco <josern1771@gmail.com>
2026-09-17 11:38:11 -0300

    Ex estrutura de repetição

---
+++ src/technobushido/maratonajava/introducao/exercicio/ExEstruturaDeRepeticao.java
@@ -0,0 +1,17 @@
+package technobushido.maratonajava.introducao.exercicio;
+
+public class ExEstruturaDeRepeticao {
+    static void main() {
+        double valorTotal = 30000;
+
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000){
+                break;
+            }
+
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+
+        }
+    }
+}
